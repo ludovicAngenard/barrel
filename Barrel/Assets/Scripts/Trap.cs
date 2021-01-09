5d65d93f893b94206dd5387c55982528622a1b94
Slygeen <64344556+Slygeen@users.noreply.github.com>
2021-01-09 18:31:18 +0100

    Create Immobility Trap

--- Barrel/Assets/Scripts/Trap.cs
+++ Barrel/Assets/Scripts/Trap.cs
@@ -1,24 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+using System;
+using System.Timers;
+
 
 public class Trap : MonoBehaviour
 {
+    public float timeRemaining = 10;
+    public bool timerIsRunning = false;
+    private FirstPersonController fpc;
+
     // Start is called before the first frame update
+
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        if (timerIsRunning)
+        {
+            if (timeRemaining > 0)
+            {
+                timeRemaining -= Time.deltaTime;
+            }
+            else
+            {
+                fpc = GameObject.FindObjectOfType<FirstPersonController>();
+
+                Debug.Log("Time has run out!");
+                timeRemaining = 0;
+                timerIsRunning = false;
+
+                Destroy(this.gameObject);
+                fpc.m_WalkSpeed = 5;
+            }
+        }
+
     }
 
-    void OnTriggerEnter(Collider other)
+    void OnCollisionEnter(Collision collision)
     {
-        Renderer render = GetComponent<Renderer>();
-        render.material.color = Color.green;
+
+        fpc = GameObject.FindObjectOfType<FirstPersonController>();
+       
+        fpc.m_WalkSpeed = 0;
+
+        timerIsRunning = true;
+      
+        
     }
 }
