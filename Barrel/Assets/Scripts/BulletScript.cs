b1588efa8b9013973c148b0abe4016bb562113ca
ludovic <ludovic.angenard@orange.fr>
2021-03-21 19:08:19 +0100

    fix the firstperson namespace

--- Barrel/Assets/Scripts/BulletScript.cs
+++ Barrel/Assets/Scripts/BulletScript.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using  NamespaceScore;
-
+using UnityStandardAssets.Characters.FirstPerson;
 
 public class BulletScript : MonoBehaviour {
 
