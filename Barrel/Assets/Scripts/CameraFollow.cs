2ffeb09f820380599f94282bb61bd788afca4cbd
ludovic <ludovic.angenard@orange.fr>
2022-02-25 10:59:43 +0100

    Add split screen and crouch

--- Barrel/Assets/Scripts/CameraFollow.cs
+++ Barrel/Assets/Scripts/CameraFollow.cs
@@ -12,11 +12,11 @@ public class CameraFollow : MonoBehaviour
     [Tooltip("GameObject you want the camera to follow")]
     public Transform target = null;
 
-    [SerializeField, Range(0.01f, 1f), Tooltip("How fast the camera follows the object")]
-    private float smoothSpeed = 0.125f;
+    [SerializeField, Range(0.01f, 0.01f), Tooltip("How fast the camera follows the object")]
+    private float smoothSpeed = 0.01f;
 
     [SerializeField, Tooltip("Camera offset from the transform target")]
-    private Vector3 offset = new Vector3(0f, 1f, 0f);
+    private Vector3 offset = new Vector3(0f, 2f, 0.5f);
 
     // Necessary for Smooth Damp function
     private Vector3 velocity = Vector3.zero;
@@ -29,8 +29,8 @@ public class CameraFollow : MonoBehaviour
     private void LateUpdate()
     {
         Vector3 desiredPosition = target.position + offset;
-
-        transform.position = Vector3.SmoothDamp(transform.position, desiredPosition, ref velocity, smoothSpeed);
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        transform.position = smoothedPosition;
 
     }
 
--- Barrel/Assets/Scripts/PlayerController.cs
+++ Barrel/Assets/Scripts/PlayerController.cs
@@ -1,40 +1,38 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using NamespaceSplitScreen;
 
 [RequireComponent(typeof(CharacterController))]
+
 public class PlayerController : MonoBehaviour
 {
+
+
     [SerializeField] private float playerSpeed = 2.0f;
-    private float sprintSpeed = 4.0f;
-    private float walkSpeed = 2.0f;
+    private float sprintSpeed = 8.0f;
+    private float walkSpeed = 4.0f;
     [SerializeField] private float jumpHeight = 1.0f;
     [SerializeField] private float gravityValue = -9.81f;
+
     private CharacterController controller;
     private Vector3 playerVelocity;
     private bool groundedPlayer;
 
-    private float crouchHeight = 0.5f;
+    private float crouchHeight = 1f;
     private float standingHeight;
-    //private float timeToCrouch = 0.25f;
-    //private Vector3 crouchingCenter = new Vector3(0, 0.5f, 0);
-    //private Vector3 standingCenter = new Vector3(0, 0, 0);
     private bool isCrouching;
-    //private bool duringCrouchAnimation;
-
     private Vector2 movementInput = Vector2.zero;
     private bool jumped = false;
 
     private bool crouch = false;
 
     private bool sprint = false;
-
-
-
-
+    [SerializeField] private GameObject playerObject;
 
     private void Start()
     {
         controller = gameObject.GetComponent<CharacterController>();
+
         standingHeight = controller.height;
 
     }
@@ -51,29 +49,20 @@ public class PlayerController : MonoBehaviour
         jumped = context.action.triggered;
     }
 
-    
-
     public void OnCrouch(InputAction.CallbackContext context)
     {
         crouch = context.action.triggered;
-
-
         CheckCrouch();
-
-
     }
 
     public void OnSprint(InputAction.CallbackContext context)
     {
         sprint = context.action.triggered;
-        Debug.Log(sprint);
-        Debug.Log("------------");
         CheckSprint();
     }
-
-
     void Update()
     {
+
         groundedPlayer = controller.isGrounded;
         if (groundedPlayer && playerVelocity.y < 0)
         {
@@ -83,42 +72,29 @@ public class PlayerController : MonoBehaviour
         Vector3 horizontalVelocity = (transform.right * movementInput.x + transform.forward * movementInput.y) * playerSpeed;
         controller.Move(horizontalVelocity * Time.deltaTime);
 
-      // Vector3 move = new Vector3(movementInput.x, 0, movementInput.y);
-      // controller.Move(move * Time.deltaTime * playerSpeed);
-
-        //if (move != Vector3.zero)
-       // {
-       //     gameObject.transform.forward = move;
-       // }
-
-        // Changes the height position of the player..
         if (jumped && groundedPlayer)
         {
             playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
         }
 
-
-        
-
         playerVelocity.y += gravityValue * Time.deltaTime;
         controller.Move(playerVelocity * Time.deltaTime);
-
-        
     }
-
 
     void CheckCrouch()
     {
-        if (crouch == true)
+        Vector3 playerScale = transform.localScale;
+
+        if (crouch)
         {
-            controller.height = crouchHeight;
+            playerScale.y = crouchHeight;
         }
         else
         {
-            controller.height = standingHeight;
+           playerScale.y = standingHeight;
         }
+        transform.localScale = playerScale;
     }
-
 
     void CheckSprint()
     {
@@ -131,5 +107,4 @@ public class PlayerController : MonoBehaviour
             playerSpeed = walkSpeed;
         }
     }
-    
 }
